812342015145b673db65386f6c0c49753152e7bf
me-AC <58519437+me-AC@users.noreply.github.com>
2019-12-04 20:27:50 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -2,6 +2,6 @@ class hello_world
 {
     public static void main(String args[])
     {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!!I am new to all This.");
     }
 }
