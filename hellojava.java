e52abd4a6b10ba35c348814879e71ec08b9575b3
ilhamhakim1205 <m.ilhamhakim@gmail.com>
2021-10-10 12:21:38 +0700

    add hellojava.java

---
+++ hellojava.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Lainlain;
+
+/**
+ * MUHAMMAD ILHAM EL HAKIM | POLINEMA
+ * @author MODERN14
+ */
+public class hellojava {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World!");
+        }
+        
+    }
+}
