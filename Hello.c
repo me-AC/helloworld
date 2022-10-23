85f01ea3df614f77b9ed69829a035d0d0a38c4bd
Urvashi90 <91781580+Urvashi90@users.noreply.github.com>
2022-10-23 16:46:59 +0530

    Added text &spaces
    made it more useful for the user

--- Hello.c
+++ Hello.c
@@ -5,18 +5,18 @@ int main()
 {
   char name[100], char ans;
   clrscr();
-  printf("Enter your name: ");
+  printf("Enter your name: \n");
   gets(name);
   printf("Hello, %s!", name);
   print("How are you %s?", name);
-  printf("Are you fine?(Yes-Y or No-N)");
+  printf("Are you fine?(Yes-Y or No-N) \n");
   scanf("%c", &ans);
   if(ans=='Y')
   {
-    printf("You are gonna be alright! You are not alone! It's Just A Bad Day! May God Bless You!");
+    printf("You are gonna be alright! You are not alone! It's Just A Bad Day! May God Bless You! we got you! \n");
   }
   else
-    printf("Glad! Have an amazing day!");
+    printf("Glad! Have an amazing day! \n");
   getch();
   return 0;
 }
